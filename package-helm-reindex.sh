da3c862f29f72bd83fd168866acd750201265494
Lauri Vorno <lauri.vorno@ixxus.com>
2019-01-29 11:42:29 +0000

    Helm repackage, fixed cloud-connector version

--- package-helm-reindex.sh
+++ package-helm-reindex.sh
@@ -2,4 +2,8 @@ helm lint charts/sample-runtime-bundle
 
 helm package charts/sample-runtime-bundle
 
+helm lint charts/sample-cloud-connector
+
+helm package charts/sample-cloud-connector
+
 helm repo index --url https://lauriv.github.io/ .
